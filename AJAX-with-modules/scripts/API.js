ec80eb6d7186cae6b64e3b0ceaa09b5cf281e07d
Nikkie Mashian <nikmash@ucla.edu>
2018-11-06 16:21:23 -0800

    refactored modules

--- AJAX-with-modules/scripts/API.js
+++ AJAX-with-modules/scripts/API.js
@@ -6,6 +6,7 @@ const API = (function(){
   const API_KEY = 'AIzaSyCRbaNBF08000E_9OCK8S8YOa8HPbBNGRE';
   const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
+  //* Async function, responsible for calling the Youtube API with jQuery, constructing the correct query object, and passing along the callback into the AJAX call
   const fetchVideos = function(searchTerm, callback) {
     const query = {
       q: searchTerm,
@@ -14,8 +15,10 @@ const API = (function(){
       maxResults: 10
     };
     $.getJSON(BASE_URL, query, callback);
+    // Make a getJSON call using the BASEURL + query object we constructed and sending the provided callback in as the last argument
   };
 
+  //This async function will be called when we hit next button so it can pass in the pageToken 
   const fetchNextVideos = function(searchTerm, callback) {
     const query = {
       q: searchTerm,
@@ -27,6 +30,7 @@ const API = (function(){
     $.getJSON(BASE_URL, query, callback);
   };
 
+  //This async function will be called when we hit previious button so it can pass in the pageToken 
   const fetchPrevVideos = function(searchTerm, callback) {
     const query = {
       q: searchTerm,
@@ -38,7 +42,10 @@ const API = (function(){
     $.getJSON(BASE_URL, query, callback);
   };
 
+  // Uses Youtube API response to create an array of "decorated" video objects
   const decorateResponse = function(response) {
+    //map through the response.items array and pick out what we want, capture it in a variable, and set our videos n store to that variable. Then render. 
+    //Also update the tokens if necassary 
     const decArr = response.items.map(item => {
       return{
         id: item.id.videoId,
@@ -47,6 +54,8 @@ const API = (function(){
         thumbnail: item.snippet.thumbnails.default.url
       };
     });
+
+    //if there's a prevToken, grab that value too and update it 
     if (response.prevPageToken){
       store.updatePrevToken(response.prevPageToken);
     }
--- AJAX-with-modules/scripts/store.js
+++ AJAX-with-modules/scripts/store.js
@@ -3,6 +3,9 @@
 // eslint-disable-next-line no-unused-vars
 const store = (function() {
   const videos = []; 
+  // We want our store to hold an array of "decorated" video objects - i.e. objects that
+  // have been transformed into ONLY the necessary data we're displaying on our page. 
+  // (Removes extraneous dataset from Youtube.)
 
   //we need to store the previous and next tokens for the buttons
   const nextToken = '';
